95fc721258d43e331537c7027338e65e2e95dcff
Regina Naslund <rnaslund66@gmail.com>
2022-11-02 15:35:57 -0700

    syntax and syntax sugar

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,59 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            if (answer < 9)
+            {
+                Console.WriteLine(answer + "is less than 9");
+            }
+            else
+            {
+                Console.WriteLine(answer + "is greater than 9");
+            }
+            //var answer = 4;// explictit typing
+            //string response = (answer < 9) ? $"{answer} is less than 9" : $"{answer} greater to or equal to 9";
+
+
+
         }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
